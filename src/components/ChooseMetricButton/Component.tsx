12b295e95e6ed1615628ae47f17d1fa306858a52
vladyslavhimself <vladyslavhimself@gmail.com>
2022-04-02 23:28:12 -0400

    MetricButton now toggler

--- src/components/ChooseMetricButton/Component.tsx
+++ src/components/ChooseMetricButton/Component.tsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import classes from './styles.module.scss';
 
+type MetricTypes = '°C' | '°F';
+
 function ChooseMetricButton(): JSX.Element {
+    const [metric, setMetric] = useState<MetricTypes>('°C');
+
+    const changeMetric = (): void => {
+        if (metric === '°C') {
+            setMetric('°F');
+        } else {
+            setMetric('°C');
+        }
+    };
+
     return (
-        <button type="button" className={classes.metrics}>
-            <span>°C / °F</span>
+        <button
+            onClick={() => changeMetric()}
+            type="button"
+            className={classes.metrics}
+        >
+            <span className={metric === '°C' ? classes.active : undefined}>
+                °C
+            </span>
+            <span style={{ margin: '0px 5px' }}>/</span>
+            <span className={metric === '°F' ? classes.active : undefined}>
+                °F
+            </span>
         </button>
     );
 }
