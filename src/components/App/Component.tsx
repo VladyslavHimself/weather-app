527a9303eca577c41f06d73d80bcd14714438dd6
vladyslavhimself <vladyslavhimself@gmail.com>
2022-04-03 19:34:08 -0400

    Moved navbar to separate component

--- src/components/App/Component.tsx
+++ src/components/App/Component.tsx
@@ -1,30 +1,10 @@
 import 'normalize.css';
-import { useState } from 'react';
-import logoSvg from '../../assets/logotype.svg';
-import ChooseMetricButton from '../ChooseMetricButton/Component';
-import SearchField from '../SearchField/Component';
-
-import classes from './styles.module.scss';
+import Navbar from '../Navbar/Component';
 
 function App() {
-    const [first, setfirst] = useState<string>('');
-
     return (
         <div className="App">
-            <div className={classes.navbar}>
-                <div className={classes.container}>
-                    <div className={classes.logotype}>
-                        <img src={logoSvg} alt="logotype" />
-                    </div>
-                    <div className={classes.search}>
-                        <SearchField
-                            stateValue={first}
-                            setStateValue={setfirst}
-                        />
-                    </div>
-                    <ChooseMetricButton />
-                </div>
-            </div>
+            <Navbar />
         </div>
     );
 }
---
+++ src/components/Navbar/Component.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import ChooseMetricButton from '../ChooseMetricButton/Component';
+import SearchField from '../SearchField/Component';
+import classes from './styles.module.scss';
+
+import logoSvg from '../../assets/logotype.svg';
+
+function Navbar(): JSX.Element {
+    const [inputField, setInputField] = useState<string>('');
+    return (
+        <div className={classes.navbar}>
+            <div className={classes.container}>
+                <div className={classes.logotype}>
+                    <img src={logoSvg} alt="logotype" />
+                </div>
+                <div className={classes.search}>
+                    <SearchField
+                        stateValue={inputField}
+                        setStateValue={setInputField}
+                    />
+                </div>
+                <ChooseMetricButton />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
