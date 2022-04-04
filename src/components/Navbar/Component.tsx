b5de56b2628a31ace54f4c7851142a86b234f0b1
vladyslavhimself <vladyslavhimself@gmail.com>
2022-04-03 20:00:01 -0400

    Added responsivnes to navbar

--- src/components/Navbar/Component.tsx
+++ src/components/Navbar/Component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ChooseMetricButton from '../ChooseMetricButton/Component';
 import SearchField from '../SearchField/Component';
 import classes from './styles.module.scss';
@@ -7,12 +7,14 @@ import logoSvg from '../../assets/logotype.svg';
 
 function Navbar(): JSX.Element {
     const [inputField, setInputField] = useState<string>('');
+
     return (
         <div className={classes.navbar}>
             <div className={classes.container}>
                 <div className={classes.logotype}>
                     <img src={logoSvg} alt="logotype" />
                 </div>
+
                 <div className={classes.search}>
                     <SearchField
                         stateValue={inputField}
