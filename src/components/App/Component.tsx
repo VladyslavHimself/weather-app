be767d61709d33d8ad0eb4c1d056edc55c80c1de
vladyslavhimself <vladyslavhimself@gmail.com>
2022-04-07 21:21:31 -0400

    added table block

--- src/components/App/Component.tsx
+++ src/components/App/Component.tsx
@@ -10,63 +10,41 @@ function App() {
             <div className={classes['today-weather']}>
                 <div className={classes.container}>
                     <div className={classes['average-data']}>
-                        <div className={classes['average-data__city']}>
-                            London
-                        </div>
-                        <div className={classes['average-data__time']}>
-                            Sunday 07:09
-                        </div>
-                        <div className={classes['average-data__temperature']}>
-                            12°C
-                        </div>
+                        <div className={classes['average-data__city']}>London</div>
+                        <div className={classes['average-data__time']}>Sunday 07:09</div>
+                        <div className={classes['average-data__temperature']}>12°C</div>
                     </div>
                     <div className={classes['general-data']}>
                         <div className={classes['data-item']}>
-                            <div className={classes['data-item__time']}>
-                                08:00
-                            </div>
+                            <div className={classes['data-item__time']}>08:00</div>
                             <div className={classes['data-item__weather-type']}>
                                 <img src={cloudySvg} alt="cloudy" />
                             </div>
-                            <div className={classes['data-item__temperature']}>
-                                13 °C
-                            </div>
+                            <div className={classes['data-item__temperature']}>13 °C</div>
                         </div>
 
                         <div className={classes['data-item']}>
-                            <div className={classes['data-item__time']}>
-                                09:00
-                            </div>
+                            <div className={classes['data-item__time']}>09:00</div>
                             <div className={classes['data-item__weather-type']}>
                                 <img src={cloudySvg} alt="cloudy" />
                             </div>
-                            <div className={classes['data-item__temperature']}>
-                                13 °C
-                            </div>
+                            <div className={classes['data-item__temperature']}>13 °C</div>
                         </div>
 
                         <div className={classes['data-item']}>
-                            <div className={classes['data-item__time']}>
-                                10:00
-                            </div>
+                            <div className={classes['data-item__time']}>10:00</div>
                             <div className={classes['data-item__weather-type']}>
                                 <img src={cloudySvg} alt="cloudy" />
                             </div>
-                            <div className={classes['data-item__temperature']}>
-                                13 °C
-                            </div>
+                            <div className={classes['data-item__temperature']}>13 °C</div>
                         </div>
 
                         <div className={classes['data-item']}>
-                            <div className={classes['data-item__time']}>
-                                11:00
-                            </div>
+                            <div className={classes['data-item__time']}>11:00</div>
                             <div className={classes['data-item__weather-type']}>
                                 <img src={cloudySvg} alt="cloudy" />
                             </div>
-                            <div className={classes['data-item__temperature']}>
-                                13 °C
-                            </div>
+                            <div className={classes['data-item__temperature']}>13 °C</div>
                         </div>
                     </div>
                 </div>
@@ -76,132 +54,55 @@ function App() {
                 <div className={classes['detailed-now-weather__hint']}>Now</div>
                 <div className={classes.container}>
                     <div className={classes['detailed-information-tab']}>
-                        <div
-                            className={
-                                classes['detailed-information-tab__title']
-                            }
-                        >
-                            Chance of Rain
-                        </div>
-                        <div
-                            className={
-                                classes['detailed-information-tab__data']
-                            }
-                        >
-                            12%
-                        </div>
+                        <div className={classes['detailed-information-tab__title']}>Chance of Rain</div>
+                        <div className={classes['detailed-information-tab__data']}>12%</div>
                     </div>
 
                     <div className={classes['detailed-information-tab']}>
-                        <div
-                            className={
-                                classes['detailed-information-tab__title']
-                            }
-                        >
-                            Humidity
-                        </div>
-                        <div
-                            className={
-                                classes['detailed-information-tab__data']
-                            }
-                        >
-                            60%
-                        </div>
+                        <div className={classes['detailed-information-tab__title']}>Humidity</div>
+                        <div className={classes['detailed-information-tab__data']}>60%</div>
                     </div>
 
                     <div className={classes['detailed-information-tab']}>
-                        <div
-                            className={
-                                classes['detailed-information-tab__title']
-                            }
-                        >
-                            Wind speed
-                        </div>
-                        <div
-                            className={
-                                classes['detailed-information-tab__data']
-                            }
-                        >
-                            10 k/h
-                        </div>
+                        <div className={classes['detailed-information-tab__title']}>Wind speed</div>
+                        <div className={classes['detailed-information-tab__data']}>10 k/h</div>
                     </div>
 
                     <div className={classes['detailed-information-tab']}>
-                        <div
-                            className={
-                                classes['detailed-information-tab__title']
-                            }
-                        >
-                            Visibility
-                        </div>
-                        <div
-                            className={
-                                classes['detailed-information-tab__data']
-                            }
-                        >
-                            100km
-                        </div>
+                        <div className={classes['detailed-information-tab__title']}>Visibility</div>
+                        <div className={classes['detailed-information-tab__data']}>100km</div>
                     </div>
 
                     <div className={classes['detailed-information-tab']}>
-                        <div
-                            className={
-                                classes['detailed-information-tab__title']
-                            }
-                        >
-                            Pressure
-                        </div>
-                        <div
-                            className={
-                                classes['detailed-information-tab__data']
-                            }
-                        >
-                            1015 hPa
-                        </div>
+                        <div className={classes['detailed-information-tab__title']}>Pressure</div>
+                        <div className={classes['detailed-information-tab__data']}>1015 hPa</div>
                     </div>
                 </div>
             </div>
             <div className={classes['other-days-weather']}>
                 <div className={classes.container}>
                     <div className={classes['weather-table']}>
-                        <div className={classes['weather-table-item']}>
-                            <div
-                                className={
-                                    classes['weather-table-item__data-title']
-                                }
-                            >
-                                Day
+                        <div className={classes['weather-table-item--title']}>
+                            <div className={classes['weather-table-item__container']}>
+                                <div className={classes['weather-table-item__data-title']}>Day</div>
+                                <div className={classes['weather-table-item__data-title']}>Chance of rain</div>
+                                <div className={classes['weather-table-item__data-title']}>Humidity</div>
+                                <div className={classes['weather-table-item__data-title']}>Wind</div>
+                                <div className={classes['weather-table-item__data-title']}>Temperature</div>
                             </div>
-                            <div
-                                className={
-                                    classes['weather-table-item__data-title']
-                                }
-                            >
-                                Chance of rain
-                            </div>
-                            <div
-                                className={
-                                    classes['weather-table-item__data-title']
-                                }
-                            >
-                                Humidity
-                            </div>
-                            <div
-                                className={
-                                    classes['weather-table-item__data-title']
-                                }
-                            >
-                                Wind
-                            </div>
-                            <div
-                                className={
-                                    classes['weather-table-item__data-title']
-                                }
-                            >
-                                Temperature
-                            </div>
+                            <hr />
                         </div>
-                        <hr />
+
+                        <div className={classes['weather-table-item']}>
+                            <div className={classes['weather-table-item__container']}>
+                                <div className={classes['weather-table-item__data-title']}>Mon</div>
+                                <div className={classes['weather-table-item__data-title']}>9%</div>
+                                <div className={classes['weather-table-item__data-title']}>57%</div>
+                                <div className={classes['weather-table-item__data-title']}>10 km/h</div>
+                                <div className={classes['weather-table-item__data-title']}>19˚</div>
+                            </div>
+                            <hr />
+                        </div>
                     </div>
                 </div>
             </div>
